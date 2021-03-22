af4f89b416bbea38afdc61e04cb78cc9be0c6872
AlbertDennisMoses <dennismoses123@yahoo.com>
2021-03-22 14:12:15 +0000

    first commit

---
+++ src/UseEffect.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+
+const UseEffect = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // async/await
+    const fetchData = async () => {
+      const result = await fetch("https://jsonplaceholder.typicode.com/users");
+      const users = await result.json();
+      console.log(users);
+      setUsers(users);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="header">USeEffect</h1>
+      {users.map((user, i) => (
+        <div className="main" key={i}>
+          <h5>
+            <strong>Name:</strong> {user.name}
+          </h5>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+          <p>
+            <strong>Username:</strong> {user.username}
+          </p>
+          <p>
+            <strong>Phone Number:</strong> {user.phone}
+          </p>
+          <p>
+            <strong>Website:</strong> {user.website}
+          </p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default UseEffect;
